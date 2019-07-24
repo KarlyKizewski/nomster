6303847f6996c4ba4e3e9a0f240dd5658324b80b
Karly Kizewski <kizewski.karly@gmail.com>
2019-07-24 18:40:44 +0000

    associations for comments and comment database table created

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments 
+  
   geocoded_by :address
   after_validation :geocode 
-  
+
   validates :name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :address, presence: true, length: { minimum: 2, maximum: 100 }
   validates :description, presence: true, length: { minimum: 2, maximum: 300 }
