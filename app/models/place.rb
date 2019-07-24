c0faa07febec0546a0599c6c4ef2c52c678cbfe1
Karly Kizewski <kizewski.karly@gmail.com>
2019-07-24 16:03:14 +0000

    validations for address and description set as well as length for all three submissions on simpleform

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :address, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 2, maximum: 300 }
 end
