f59bf1222b7370f25043bd6ea61571be66b4a12e
Karly Kizewski <kizewski.karly@gmail.com>
2019-07-24 17:05:34 +0000

    geocoder and figaro gems with geocoder saved

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode 
+  
   validates :name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :address, presence: true, length: { minimum: 2, maximum: 100 }
   validates :description, presence: true, length: { minimum: 2, maximum: 300 }
